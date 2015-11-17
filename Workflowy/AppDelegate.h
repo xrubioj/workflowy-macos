88bab73b410a47a2d509725ec80f2d59d22ee652
Xavier Rubio Jansana <xavier@toro-intl.com>
2015-11-17 21:23:42 +0100

    Initial Commit

---
+++ Workflowy/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Workflowy
+//
+//  Created by Xavier Rubio Jansana on 17/11/15.
+//  Copyright © 2015 Apps Ezy. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Workflowy/AppDelegate.m
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.m
+//  Workflowy
+//
+//  Created by Xavier Rubio Jansana on 17/11/15.
+//  Copyright © 2015 Apps Ezy. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+@end
